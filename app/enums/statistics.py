4626cd2f0fd9f6c055a164d6e91eca31617c5036
Volodymyr Tkach <vovantkac5@gmail.com>
2025-12-02 22:22:32 +0200

    fixes of api

--- app/enums/statistics.py
+++ app/enums/statistics.py
@@ -2,6 +2,6 @@ from enum import StrEnum
 
 
 class StatisticsPeriod(StrEnum):
-    LAST_7_DAYS = "last_7_days"
-    LAST_30_DAYS = "last_30_days"
-    LAST_YEAR = "last_year"
+    LAST_7_DAYS = "LAST_7_DAYS"
+    LAST_30_DAYS = "LAST_30_DAYS"
+    LAST_YEAR = "LAST_YEAR"
